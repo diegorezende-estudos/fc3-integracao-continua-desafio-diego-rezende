868ea17e5845cdb21bc509b9e996d3c76b26278f
Diego Rezende <207320411+diegorezende-estudos@users.noreply.github.com>
2025-04-12 19:06:37 +0200

    Fix functions´name at sub.js

--- src/sub.js
+++ src/sub.js
@@ -2,23 +2,23 @@ function sub(a,b){
     return a - b;
 }
 
-function sub(a,b,c){
+function subThree(a,b,c){
     return a - b - c;
 }
 
-function sub(a,b,c,d){
+function subFour(a,b,c,d){
     return a - b - c - d;
 }
 
-function sub(a,b,c, d, e){
+function subFive(a,b,c, d, e){
     return a - b - c - d - e;
 }
 
-function sub(a,b, c, d, e, f){
+function subSix(a,b, c, d, e, f){
     return a - b - c - d - e - f;
 }
 
-function sub(a,b, c, d, e, f, g){
+function subSeven(a,b, c, d, e, f, g){
     return a - b - c - d - e - f - g;
 }
 module.exports= sub;
