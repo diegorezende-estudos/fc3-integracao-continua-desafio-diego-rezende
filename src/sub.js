1f897c11aeeb74c1d69e82f9743589142a964312
Diego Rezende <207320411+diegorezende-estudos@users.noreply.github.com>
2025-04-12 19:03:03 +0200

    Force status check to forbid merge due low coverage

--- src/sub.js
+++ src/sub.js
@@ -2,4 +2,23 @@ function sub(a,b){
     return a - b;
 }
 
+function sub(a,b,c){
+    return a - b - c;
+}
+
+function sub(a,b,c,d){
+    return a - b - c - d;
+}
+
+function sub(a,b,c, d, e){
+    return a - b - c - d - e;
+}
+
+function sub(a,b, c, d, e, f){
+    return a - b - c - d - e - f;
+}
+
+function sub(a,b, c, d, e, f, g){
+    return a - b - c - d - e - f - g;
+}
 module.exports= sub;
